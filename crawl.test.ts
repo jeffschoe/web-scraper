7eade4a530c18a945d8ae4e2e7a1c3faaa63643e
jeffschoe <ratacame@protonmail.com>
2026-03-18 21:45:48 -0400

    CH1: Setup, L4: Extract Page Content

--- crawl.test.ts
+++ crawl.test.ts
@@ -1,6 +1,11 @@
 import { test, expect } from "vitest";
-import { normalizeURL } from "./crawl";
+import { 
+  getHeadingFromHTML, 
+  normalizeURL,
+  getFirstParagraphFromHTML,
+} from "./crawl";
 
+// normalizeURL
 test("normalizeURL protocol", () => {
   const input = "https://crawler-test.com/path";
   const actual = normalizeURL(input);
@@ -26,5 +31,157 @@ test("normalizeURL http", () => {
   const input = "http://CRAWLER-TEST.com/path";
   const actual = normalizeURL(input);
   const expected = "crawler-test.com/path";
+  expect(actual).toEqual(expected);
+});
+
+
+// getHeadingFromHTML
+test("getHeadingFromHTML h1 basic", () => {
+  const inputBody = `<html><body><h1>Test Title</h1></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "Test Title";
+  expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML h2 basic", () => {
+  const inputBody = `<html><body><h2>Test Heading 2</h2></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "Test Heading 2";
+  expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML h1 and h2 basic", () => {
+  const inputBody = `<html><body><h1>Test Title</h1><h2>Test Heading 2</h2></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "Test Title";
+  expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML missing h1 and h2", () => {
+  const inputBody = `<html><body></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = '';
+  expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML empty h1", () => {
+  const inputBody = `<html><body><h1></h1></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "";
+  expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML empty h2", () => {
+  const inputBody = `<html><body><h2></h2></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "";
+  expect(actual).toEqual(expected);
+});
+
+
+// getFirstParagraphFromHTML
+test("getFirstParagraphFromHTML main one paragraph", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph.</p>
+      <main>
+        <p>Main paragraph.</p>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Main paragraph.";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML main two paragraphs", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph.</p>
+      <main>
+        <p>Main paragraph.</p>
+        <p>Main paragraph 2.</p>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Main paragraph.";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML main two paragraphs, two outside paragraphs", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph 1.</p>
+      <p>Outside paragraph 2.</p>
+      <main>
+        <p>Main paragraph.</p>
+        <p>Main paragraph 2.</p>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Main paragraph.";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML main without paragraph, one outside paragraph", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph.</p>
+      <main>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Outside paragraph.";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML main without paragraph, two outside paragraph", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph 1.</p>
+      <p>Outside paragraph 2.</p>
+      <main>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Outside paragraph 1.";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML main missing, two outside paragraph", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph 1.</p>
+      <p>Outside paragraph 2.</p>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Outside paragraph 1.";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML no paragraphs, main present", () => {
+  const inputBody = `
+    <html><body>
+      <main>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML no paragraphs, no main", () => {
+  const inputBody = `
+    <html><body>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "";
   expect(actual).toEqual(expected);
 });
--- crawl.ts
+++ crawl.ts
@@ -1,3 +1,4 @@
+import { JSDOM }  from 'jsdom';
 
 
 
@@ -10,4 +11,28 @@ export function normalizeURL(url: string) {
     fullPath = fullPath.slice(0, -1);
   }
   return fullPath; 
+}
+
+export function getHeadingFromHTML(html: string): string {
+  try {
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+    const heading = doc.querySelector("h1") ?? doc.querySelector("h2"); // The ?? (nullish coalescing) operator means "use the left side if it's not null/undefined, otherwise use the right side."
+    return (heading?.textContent ?? "").trim(); // The ?. is optional chaining. If h1 is null or undefined, this short-circuits and returns undefined instead of throwing an error.
+  } catch {
+    return "";
+  }
+}
+
+export function getFirstParagraphFromHTML(html: string): string {
+  try {
+    const dom = new JSDOM(html);
+    const doc = dom.window.document;
+
+    const main = doc.querySelector("main");
+    const p = main?.querySelector("p") ?? doc.querySelector("p"); // `main?` short-circuits the left side to undefined, in the case of main being null, which gives us the right side. Without it, we would throw an error on the expression itself.
+    return (p?.textContent ?? "").trim();
+  } catch {
+    return "";
+  }
 }
