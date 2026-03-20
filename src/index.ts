a819a0b7be8370954459655696c2fa7b78843ce2
jeffschoe <ratacame@protonmail.com>
2026-03-20 14:41:55 -0400

    CH2: Crawling, L1: Main

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-console.log("Hello, World!")
+import { argv } from "node:process";
+
+
+
+
+
+function main() {
+
+
+  const args = argv;
+
+  /*
+  console.log(`args.length = ${args.length}`)
+  argv.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+  }); 
+
+  const arr = ["A", "B", "C"]
+  arr.forEach(
+    (element) => {
+      const lower = element.toLowerCase();
+      console.log(`element = ${lower}`);
+    }
+    
+  );
+  */
+
+  const argsLen = args.length;
+  if (argsLen < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  }
+  if (argsLen > 3) {
+    console.log("too many arguments provided");
+    process.exit(1);
+  } 
+
+  const baseURL = process.argv[2];
+
+  console.log(`starting crawl of: ${baseURL}...`);
+
+  process.exit(0);
+  
+
+}
+
+
+
+main ();
