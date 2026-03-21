72ab6203a1d03a1fc3471b414fed9d96050592e7
jeffschoe <ratacame@protonmail.com>
2026-03-21 15:18:32 -0400

    CH2: Crawling, L2: Get HTML

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,11 @@
 import { argv } from "node:process";
+import { getHTML } from "./crawl";
 
 
-
-
-
-function main() {
-
-
+async function main() {
   const args = argv;
-
-  /*
-  console.log(`args.length = ${args.length}`)
-  argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
-  }); 
-
-  const arr = ["A", "B", "C"]
-  arr.forEach(
-    (element) => {
-      const lower = element.toLowerCase();
-      console.log(`element = ${lower}`);
-    }
-    
-  );
-  */
-
   const argsLen = args.length;
+ 
   if (argsLen < 3) {
     console.log("no website provided");
     process.exit(1);
@@ -34,14 +14,13 @@ function main() {
     console.log("too many arguments provided");
     process.exit(1);
   } 
-
   const baseURL = process.argv[2];
 
   console.log(`starting crawl of: ${baseURL}...`);
 
-  process.exit(0);
+  const html = await getHTML(baseURL);
+  console.log(html);
   
-
 }
 
 
